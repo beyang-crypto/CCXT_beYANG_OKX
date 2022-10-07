50cb2b28e22d505731660a5f756560f838003671
TestingAccMar <114166594+TestingAccMar@users.noreply.github.com>
2022-10-07 18:21:54 +0400

    Delete okx directory

--- okx/ws/process.go
+++
@@ -1,9 +0,0 @@
-package ws
-
-func (b *OKXWS) processTickers(symbol string, data Tickers) {
-	b.Emit(ChannelTicker, symbol, data)
-}
-
-func (b *OKXWS) processWalletBalance(data WalletBalance) {
-	b.Emit(ChannelBalanceAndPosition, data)
-}
